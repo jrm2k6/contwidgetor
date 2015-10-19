8865d5d2a5c20cab0af5cf45b038b12a3176dd85
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-10-18 22:53:19 -0700

    get rid of the loki map reduce function

--- src/utils/timeline-utils.js
+++ src/utils/timeline-utils.js
@@ -1,20 +1,19 @@
 var _     = require('lodash');
 
-    var mapTimestamp = function(obj) {
-        return obj.timestamp;
-    };
-
-    var reduceToSameDay = function(timestamps) {
-        return _.groupBy(timestamps, function(timestamp) {
-            return timestamp.split('T')[0];
-        });
-    }
 var getCommitsPerDay = function(_commitsCollection, _provider) {
-
-    commitsPerDay = _commitsCollection.mapReduce(mapTimestamp, reduceToSameDay);
-    return _.map(commitsPerDay, function(item, key) {
-        return {'date': key, 'num_commits': item.length};
+    var commitsForProvider = _commitsCollection.where(function(item) {
+        return item.provider === _provider;
     });
+
+    var commitsPerDay = _.chain(commitsForProvider).map(function(item) {
+        return item.timestamp;
+    }).groupBy(function(item) {
+        return item.split('T')[0];
+    }).map(function(item, key) {
+        return {'date': key, 'num_commits': item.length};
+    }).value();
+
+    return commitsPerDay;
 }
 
 module.exports = {
