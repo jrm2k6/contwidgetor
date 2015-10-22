f5ab5454096270b27fedd43da85207f4df37a74a
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-10-22 09:05:14 -0700

    pass props to react component

--- browser.js
+++ browser.js
@@ -3,5 +3,6 @@ require('babel/register');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./src/ui/grid');
+var _contributions = require('./dummy-data.json');
 
-ReactDOM.render(React.createElement(App), document.body);
+ReactDOM.render(React.createElement(App, {contributions: _contributions}), document.body);
--- src/ui/grid.js
+++ src/ui/grid.js
@@ -7,9 +7,25 @@ var {
 } = React;
 
 var App = React.createClass({
+    getInitialState: function() {
+        return {
+            contributions: []
+        };
+    },
+
+    componentWillMount: function() {
+        let contributionsProps = this.props.contributions;
+        let _contributions = Object.keys(contributionsProps).map(
+            item => [item, contributionsProps[item]]
+        );
+
+        this.setState({'contributions': _contributions});
+    },
+
     render: function() {
+        let nbContributions = this.state.contributions.length;
         return (
-            <div>Grid</div>
+            <div>Grid {nbContributions}</div>
         );
     }
 });
