9735833aec0485843c172d3448d167d64893a789
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-11-05 09:01:01 -0800

    update style index.html to make sure grid is fully visible

--- browser.js
+++ browser.js
@@ -5,4 +5,4 @@ var ReactDOM = require('react-dom');
 var App = require('./src/ui/grid');
 var _contributions = require('./dummy-data.json');
 
-ReactDOM.render(React.createElement(App, {contributions: _contributions}), document.body);
+ReactDOM.render(React.createElement(App, {contributions: _contributions}), document.getElementById('react-content'));
