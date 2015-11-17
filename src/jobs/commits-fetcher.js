60d5c30805a44c364e7df5daf2f01c94bc6be729
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-11-17 10:09:51 -0800

    fix name

--- src/jobs/commits-fetcher.js
+++ src/jobs/commits-fetcher.js
@@ -2,7 +2,7 @@ var bitbucketCommitsFetcher = require('../data/bitbucket-commits-fetcher');
 var githubCommitsFetcher = require('../data/github-commits-fetcher');
 
 var run = function() {
-    bitbucketCommitsFetcher.run(githubAuth.run);
+    bitbucketCommitsFetcher.run(githubCommitsFetcher.run);
 }
 
 module.exports = {
