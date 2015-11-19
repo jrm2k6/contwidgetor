fee18efdad64e66f726cc3c0924430d4999f6a55
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-11-19 09:14:07 -0800

    load contributions from json file and returns html markup

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var ReactDOMServer = require('react-dom/server');
 var ContributionsGrid = require('./src/ui/grid');
 var CommitsFetcher = require('./src/jobs/commits-fetcher');
 var TimelineGenerator = require('./src/data/timeline-generator');
+require('dotenv').load({path: process.cwd() + '/.env'});
 
 module.exports = {
-    getStaticMarkup: function() {ReactDOMServer.renderToStaticMarkup(React.createElement(ContributionsGrid));},
+    getStaticMarkup: function() {
+        var _contributions = require(process.cwd() + '/' + process.env.JSON_FILENAME + '.json');
+        return ReactDOMServer.renderToStaticMarkup(React.createElement(ContributionsGrid, {contributions: _contributions}));
+    },
     fetchContributions: CommitsFetcher.run,
     getContributionsTimeline: TimelineGenerator.generateCommitsTimeline
 };
