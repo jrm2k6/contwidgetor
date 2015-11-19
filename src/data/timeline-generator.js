84fdf1859ee51af39536f32538c2ad72cddf1028
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-11-19 09:03:19 -0800

    refactor timeline-generator to make sure db is fully loaded before reading from it

--- src/data/timeline-generator.js
+++ src/data/timeline-generator.js
@@ -5,31 +5,36 @@ var timelineUtils = require('../utils/timeline-utils');
 require('dotenv').load({path: appRoot + '/.env'});
 
 var generateCommitsTimeline = function() {
-    var db = require('../db/content-provider');
-    db.loadDatabase({}, function() {
-        var outputFilename = process.env.JSON_FILENAME + '.json';
-        var commits = db.getCollection('commits');
-        var githubCommits = timelineUtils.getCommitsPerDay(commits, 'github');
-        var bitbucketCommits = timelineUtils.getCommitsPerDay(commits, 'bitbucket');
-        var allCommits = githubCommits.concat(bitbucketCommits);
+    let afterDbLoaded = () => {
+        var db = require('../db/content-provider').getDB();
+        db.loadDatabase({}, function() {
+            var outputFilename = process.env.JSON_FILENAME + '.json';
+            var commits = db.getCollection('commits');
+            var githubCommits = timelineUtils.getCommitsPerDay(commits, 'github');
+            var bitbucketCommits = timelineUtils.getCommitsPerDay(commits, 'bitbucket');
+            var allCommits = githubCommits.concat(bitbucketCommits);
 
-        var sortedCommitsByDate = allCommits.sort(function(first, second) {
-            return new Date(first.date) - new Date(second.date);
+            var sortedCommitsByDate = allCommits.sort(function(first, second) {
+                return new Date(first.date) - new Date(second.date);
+            });
+
+            var jsonData = sortedCommitsByDate.reduce(function(acc, item) {
+                var currentDate = item.date;
+                if (acc[currentDate] != null) {
+                    acc[currentDate] += item.num_commits;
+                } else {
+                    acc[currentDate] = item.num_commits;
+                }
+
+                return acc;
+            }, {});
+
+            fs.writeFileSync(outputFilename, JSON.stringify(jsonData));
         });
+    };
 
-        var jsonData = sortedCommitsByDate.reduce(function(acc, item) {
-            var currentDate = item.date;
-            if (acc[currentDate] != null) {
-                acc[currentDate] += item.num_commits;
-            } else {
-                acc[currentDate] = item.num_commits;
-            }
-
-            return acc;
-        }, {});
-
-        fs.writeFileSync(outputFilename, JSON.stringify(jsonData));
-    });
+    const ContentProvider = require('../db/content-provider');
+    ContentProvider.createOrLoadDatabase(afterDbLoaded);
 }
 
 module.exports = {
