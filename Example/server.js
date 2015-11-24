014a3f8bcbf4719ae07200dac65009279779451c
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-11-24 15:32:02 -0800

    modify example

--- Example/server.js
+++ Example/server.js
@@ -1,16 +1,14 @@
 var express = require('express');
+var contwidgetor = require('contwidgetor');
+
 var app = express();
-var System = require('es6-module-loader').System;
-System.transpiler = 'babel';
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/templates');
 
 
 app.get('/', function(request, response) {
-  System.import('contwidgetor').then(function(m) {
-    console.log(m);
-  });
+  res.render('index');
 });
 
 var port = process.env.PORT || 5000;
