3111ab69dd4b7b0f6ca133ae2ee371cc718c9ca9
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-11-24 17:17:18 -0800

    fix example

--- Example/server.js
+++ Example/server.js
@@ -3,12 +3,13 @@ var contwidgetor = require('contwidgetor');
 
 var app = express();
 
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/templates');
 
 
 app.get('/', function(request, response) {
-  res.render('index');
+  var _contributionsGridElem = contwidgetor.getContributionsGridWidget();
+  response.render('index.ejs', {contributionsGrid: _contributionsGridElem});
 });
 
 var port = process.env.PORT || 5000;
